21eca6217ededb8e188b3026166ba245612e9258
sarajsadanand <sarajsadanand@hotmail.com>
2022-09-08 22:56:51 +0530

    POST request from json added and using postman sent a POST req

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)  # name will import all the named functions
@@ -7,8 +6,15 @@ app = Flask(__name__)  # name will import all the named functions
 # get means sending a data/searchquery through URL, e.g. google search
 # whereas POST means sending data through the body in a way data is not visible to the user e.g. login using password we cannot see it in URL
 @app.route('/xyz', methods=['GET', 'POST'])
-def test(a, b):
-    return a + b
+def test():
+    if request.method == 'POST':
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a + b
+        return jsonify(str(result))
 
 
-print(test(1, 2))
+if __name__ == '__main__':
+    app.run()
+
+# error 400 means an issue from client side (postman), 404 server down, and error 500 means an issue from the server side, 200 is successful
